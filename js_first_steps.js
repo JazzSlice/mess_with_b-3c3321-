e8eecd9bd3f57c4afc4743ad376f446ecaec9834
JazzSlice <108577355+JazzSlice@users.noreply.github.com>
2023-02-16 01:53:50 +0300

    Create js_first_steps
    initialise mess

---
+++ js_first_steps.js
@@ -0,0 +1,15 @@
+// netology start course
+
+let a = 5;
+let b = 7;
+let sum = a + b;
+
+console.log(`Итог: ${a} + ${b} = ${sum} y.e.`);
+
+let username = 'my name';
+let bonusBalance = 1000;
+let increasePerBuy =  50;
+let lostPerDay = 3;
+
+console.log(`Пользователь ${username}`);
+console.log(`Баланс ${bonusBalance + (lostPerDay + (6*lostPerDay + 6*increasePerBuy))}`);
