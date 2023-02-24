ce32eaddd23ef7baacca9dd3297b0bfb4bb82fbb
JazzSlice <108577355+JazzSlice@users.noreply.github.com>
2023-02-24 14:44:21 +0300

    Add fibo foo base
    compare with formula step 5

--- non_linear_stuff.py
+++ non_linear_stuff.py
@@ -8,6 +8,7 @@ b = -3
 a1 = a
 b1 = b
 
+
 fibo_arr = [1, 1]
 
 def getFiboNum (k):
@@ -49,8 +50,8 @@ def getGoldenRatioRes (a, b):
 
     i2target = (math.log((b1 - a1) / target) / math.log(teta))
 
-    print("_______GOLDEN_RATIO_RESULT_______")
-    print("x in bounds of [{:}:{:}]\nResult is: {: >37}".format(a, b, ((a + b) / 2)))
+    print("__________________GOLDEN_RATIO_RESULT_________________")
+    print("x in bounds of                 [{:}:{:}]\nResult is: {: >37}".format(a, b, ((a + b) / 2)))
     print('Iterations calculated: {: >9} \nIterations for target: {: >9}'.format(k, -math.floor(i2target)))
     print('Length by calculated values: {: >10.5} \nLength by formula: {: >17.5}'.format(b-a, target/(b1-a1)))
 
@@ -66,13 +67,41 @@ def getDixotomicRes (a, b):
             a = Mu
         else:
             b = Lambda
-    k += 1
+    # k += 1
     i2target = (math.log((b1 - a1) / target) / math.log(2))
     
-    print("________DIXOTOMIC_RESULT_________")
-    print("x in bounds of [{:}:{:}]\nResult is: {: >27}".format(a, b, ((a + b) / 2)))
-    print('Iterations calculated: {: >9} \nIterations for target: {: >9}'.format(k, math.floor(i2target)))
-    print('Length by calculated values: {: >8.5} \nLength by formula: {: >17.5}'.format(b-a, eps/(b1-a1)))
+    print("___________________DIXOTOMIC_RESULT___________________")
+    print("x in bounds of                 [{:}:{:}]\nResult is: {: >27}".format(a, b, ((a + b) / 2)))
+    print('Iterations calculated: {: >9} \nIterations for target: {: >9}'.format(k, math.trunc(i2target)))
+    print('Length by calculated values: {: >8.5} \nLength by formula: {: >17.5}'.format(b-a, target/(b1-a1)))
+
+def getFiboRes ():
+    k = 0
+    i2target = ((b1 - a1) / target)
+    while k < i2target:
+        k += 1
+        Lambda = (a + (getFiboNum(len(fibo_arr) - k - 1) / getFiboNum(len(fibo_arr) - k + 1) * (b - a)))
+        Mu = (a + (getFiboNum(len(fibo_arr) - k) / getFiboNum(len(fibo_arr) - k + 1) * (b - a)))
+        FLamb = getFooRes(Lambda, 1)
+        FMu = getFooRes(Mu, 1)
+        if FLamb > FMu:
+            if k != (len(fibo_arr) - 2):
+                a = Lambda
+                Lambda = Mu
+                Mu = (a + (getFiboNum(len(fibo_arr) - k - 1) / getFiboNum(len(fibo_arr) - k) * (b - a)))
+                FMu = getFooRes(Mu, 1)
+            else:
+                break
+        else:
+            if k != (len(fibo_arr) - 2):
+                b = Mu
+                Mu = Lambda
+                Lambda = (a + (getFiboNum(len(fibo_arr) - k - 2) / getFiboNum(len(fibo_arr) - k) * (b - a)))
+                FLamb = getFooRes(Lambda, 1)
+            else:
+                break
+        Lambda = 
+        # check case: out of fibo_arr 'n' index in 'k != ' condition
 
 
 getGoldenRatioRes (a, b)
