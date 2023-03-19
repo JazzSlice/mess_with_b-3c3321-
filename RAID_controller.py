a913779079b289cefab414c498a61eb000b75f1a
JazzSlice <e308e18@gmail.com>
2023-03-19 22:24:45 +0300

    update RAID controller

--- RAID_controller.py
+++ RAID_controller.py
@@ -1,20 +1,39 @@
 print(f'{"RAID - controller (6)":_^20}')
-disk_num = int(input())
-disk_v = int(input())
-data_kit_v = int(input())
 
-data_vpd = data_kit_v / (disk_num - 2)
-if int(data_vpd) != data_vpd:
-    data_vpd = int(data_vpd) + 1
-elif data_vpd > disk_v:
-    print('Overweight data_kit')
+layers = {}
+data_kit_v = -1
+i = 1
+while True:
 
-logical_disk = []
-for i in range(len(logical_disk)):
-    if i % 2 == 0:
-        logical_disk[i - 1] = 0.01
-    else:
-        logical_disk[i - 1] = data_vpd
+    txt = input('1. Add data kit\n2. Show logic-disk status\n3. Break\n')
+    match txt:
+        case '1':
+            if data_kit_v < 0:
+                disk_num = 5 #int(input(Enter number of disks: ))
+                disk_v = 120 #int(input(Enter disk volume: ))
 
-print(logical_disk)
-    
+            data_kit_v = 80 #int(input(Enter data-kit volume: ))
+            data_vpd = data_kit_v / (disk_num - 2)
+            if int(data_vpd) != data_vpd:
+                data_vpd = int(data_vpd) + 1
+
+            if data_vpd > disk_v:
+                print('Overweight data_kit')
+                break
+
+            logical_disk = []
+            for j in range(disk_num):
+                if j % 2 != 0:
+                    logical_disk.append(0.01)
+                else:
+                    logical_disk.append(data_vpd)
+
+            layers[f'{i}'] = logical_disk
+            i += 1
+        case '2':
+            
+            for i in range(len(logical_disk)):
+                print(logical_disk[i])
+            print(layers)
+        case '3':
+            break
