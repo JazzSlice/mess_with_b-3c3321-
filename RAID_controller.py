627b374797e6cab0525846ffe4ea8087cc3ff5c5
JazzSlice <e308e18@gmail.com>
2023-03-20 23:59:23 +0300

    update README & RAID

--- RAID_controller.py
+++ RAID_controller.py
@@ -1,39 +1,74 @@
 print(f'{"RAID - controller (6)":_^20}')
 
-layers = {}
+logical_disk = {}
 data_kit_v = -1
 i = 1
+disk_num = 5 #int(input(Enter number of disks: ))
+disk_v = 120 #int(input(Enter disk volume: ))
+index = disk_num
+
+class disk:
+    def __init__(self, num, value):
+        self.num = num + 1
+        self.memory = value
+        self.layers = []
+
+    def add_layer(self, data_value):
+        free_memory = self.memory - self.count_busy_memory()
+        if free_memory < data_value:
+            print(f'Overweight data_kit for disk {self.num}')
+            return
+        else:
+            self.layers.append(data_value)
+    
+    def count_busy_memory(self):
+        x = 0
+        for i in range(len(self.layers)):
+            x += self.layers[i]
+        # result = self.memory - x
+        return x
+    
+    def print_memory_usage(self):
+        # index_free = self.memory - self.count_free_memory()
+        # index_busy = 1 - index_free
+        print(f'Disk {self.num} | {self.count_busy_memory()} / {self.memory}')# + int(10 * index_busy) * '-' + int(10 * index_free) * '_')
+
+
+for i in range(disk_num):
+    logical_disk[f'{i}'] = disk(i, disk_v)
+
 while True:
 
     txt = input('1. Add data kit\n2. Show logic-disk status\n3. Break\n')
     match txt:
         case '1':
-            if data_kit_v < 0:
-                disk_num = 5 #int(input(Enter number of disks: ))
-                disk_v = 120 #int(input(Enter disk volume: ))
-
             data_kit_v = 80 #int(input(Enter data-kit volume: ))
             data_vpd = data_kit_v / (disk_num - 2)
+            
             if int(data_vpd) != data_vpd:
-                data_vpd = int(data_vpd) + 1
+                data_vpd = abs(data_vpd) + 1
 
             if data_vpd > disk_v:
                 print('Overweight data_kit')
                 break
 
-            logical_disk = []
             for j in range(disk_num):
-                if j % 2 != 0:
-                    logical_disk.append(0.01)
+                if j == (index - 1) or j == index:
+                    logical_disk[f'{j}'].add_layer(0.01)
                 else:
-                    logical_disk.append(data_vpd)
+                    logical_disk[f'{j}'].add_layer(data_vpd)
 
-            layers[f'{i}'] = logical_disk
+                if j == range(disk_num):
+                    if (index - 2) > 0:
+                        index -= 1
+                    else:
+                        index = disk_num
+                
+
             i += 1
         case '2':
-            
-            for i in range(len(logical_disk)):
-                print(logical_disk[i])
-            print(layers)
+            for j in range(len(logical_disk)):
+                disk_pointer = logical_disk[f'{j}']
+                print(disk_pointer.print_memory_usage())
         case '3':
             break
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # mess_with_b-3c3321-
+
+This repo is created to do some study projects for university
+Here's:
+- optimisation (directory for learn non-linear algos)
+- test (to automize workflow)
+- ya (to study new features in py-lang)
+- conference (prepare multitasking high-load cluster system with use of non-linear algos while balancing load)
